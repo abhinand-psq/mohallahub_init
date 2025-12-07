573b946a72e037f7098c75bf9b996a129dc7f02e
abhinand-psq <103823872+abhinand-psq@users.noreply.github.com>
2025-12-07 10:39:42 +0530

    sample

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173' || 'http://localhost:3000',
+  origin: 'https://mohalla-frontend.vercel.app' || 'http://localhost:3000',
   credentials: true
 }));
 app.use(express.json());
